d80495ea3f1e59c9459a7c013f8c68f5daaad351
Titaree Jumnongnusorn <96689258+gaillttr@users.noreply.github.com>
2023-01-18 02:07:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,26 +14,6 @@ function myFunction() {
     }
   }
 
-// let slideIndex = 0;
-// showSlides();
-  
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-
 let slideIndex = [1,1];
 let slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
